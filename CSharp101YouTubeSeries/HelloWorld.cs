01ac880b5602083abc556ac548c770f4e7fa5c3c
David Guimarães <davidguimaraes@digix.com.br>
2021-01-12 23:43:50 -0400

    02 - Hello World - Experimenting the different ways of declaring a Hello World in C#.

---
+++ CSharp101YouTubeSeries/HelloWorld.cs
@@ -0,0 +1,63 @@
+﻿//
+// using System;
+//
+// class HelloWorld
+// {
+//     static void Main()
+//     {
+//         Console.WriteLine("Hello World, C#!");
+//     }
+// }
+
+// using System;
+//
+// public class HelloWorld
+// {
+//     public static void Main()
+//     {
+//         Console.WriteLine("Hello World, C#!");
+//     }
+// }
+
+// using System;
+//
+// public class HelloWorld
+// {
+//     public static void Main(string[] args)
+//     {
+//         Console.WriteLine("Hello World, C#!");
+//     }
+// }
+
+// using System;
+//
+// namespace CSharp101YouTubeSeries
+// {
+//     public class HelloWorld
+//     {
+//         public static void Main(string[] args)
+//         {
+//             Console.WriteLine("Hello World, C#!");
+//         }
+//     }
+// }
+
+using System;
+
+namespace CSharp101YouTubeSeries
+{
+    public class HelloWorld
+    {
+        public static void Main(string[] args)
+        {
+            // Console.WriteLine("Hello World!");
+            // Console.WriteLine("Hello Friends!");
+
+            // string aFriend = "Kendra";
+            string aFriend = "Scott";
+            // Console.WriteLine(aFriend);
+            // Console.WriteLine($"Hello, " + aFriend);
+            Console.WriteLine($"Hello, {aFriend}");
+        }
+    }
+}
