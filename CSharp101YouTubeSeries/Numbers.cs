9752dadf0eefe75cd1f9bb7aef2a79dbff8e7575
David Guimarães <davidguimaraes@digix.com.br>
2021-01-13 20:15:54 -0400

    05 - Numbers and Integer Math

---
+++ CSharp101YouTubeSeries/Numbers.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace CSharp101YouTubeSeries
+{
+    public class Numbers
+    {
+        public static void Main(string[] args)
+        {
+            // int a = 10, b = 5, c = a + b;
+            // int a = 10;
+            // int b = 5;
+            // int c = a + b;
+            // int d = a - b;
+            // int e = a * b;
+            // int f = a / b;
+            // int g = a % b;
+            //
+            // Console.WriteLine(c);    //15
+            // Console.WriteLine(d);    //5
+            // Console.WriteLine(e);    //50
+            // Console.WriteLine(f);    //2
+            // Console.WriteLine(g);    //0
+            
+            int a = 10;
+            int b = 5;
+            int c = 2;
+            int d = a + b * c;
+            int e = (a + b) * c;
+            
+            Console.WriteLine(d);    //20
+            Console.WriteLine(e);    //30
+        }
+    }
+}
--- CSharp101YouTubeSeries/Strings.cs
+++ CSharp101YouTubeSeries/Strings.cs
@@ -5,61 +5,61 @@ namespace CSharp101YouTubeSeries
 {
     public class Strings
     {
-        public static void Main(string[] args)
-        {
-            // string firstFriend = "Scott";
-            // string secondFriend = "Kendra";
-            //
-            // Console.WriteLine($"My friends are {firstFriend} and {secondFriend}");
-            //
-            // Console.WriteLine($"The name {firstFriend} has {firstFriend.Length} letters");
-            // Console.WriteLine($"The name {secondFriend} has {secondFriend.Length} letters");
-
-            // string greeting = "     Hello, World!     ";
-            // Console.WriteLine($"[{greeting}]");
-            //
-            // string trimmedGreeting = greeting.TrimStart();
-            // Console.WriteLine($"[{trimmedGreeting}]");
-            //
-            // trimmedGreeting = greeting.TrimEnd();
-            // Console.WriteLine($"[{trimmedGreeting}]");
-            //
-            // trimmedGreeting = greeting.Trim();
-            // Console.WriteLine($"[{trimmedGreeting}]");
-
-            // string sayHello = "Hello, World!";
-            // Console.WriteLine(sayHello);
-            // sayHello = sayHello.Replace("Hello", "Greetings");
-            // Console.WriteLine(sayHello);
-            //
-            // sayHello = sayHello.Replace("Something else", "Greetings");
-            // Console.WriteLine(sayHello);
-            //
-            // Console.WriteLine(sayHello.ToUpper());
-            // Console.WriteLine(sayHello.ToLower());
-
-            // string songLyrics = "You say goodbye, and I say hello";
-            //
-            // Console.WriteLine(songLyrics.Contains("goodbye"));
-            // Console.WriteLine(songLyrics.Contains("greetings"));
-            //
-            // string newSongLyrics = "You say greetings, and I say hello";
-            //
-            // Console.WriteLine(newSongLyrics.Contains("goodbye"));
-            // Console.WriteLine(newSongLyrics.Contains("greetings"));
-            
-            string songLyrics = "You say goodbye, and I say hello";
-            
-            Console.WriteLine(songLyrics.StartsWith("You"));
-            Console.WriteLine(songLyrics.EndsWith("hello"));
-            
-            Console.WriteLine(songLyrics.StartsWith("you"));
-            Console.WriteLine(songLyrics.EndsWith("hello"));
-
-            var firstSubString = "You";
-            var secondSubString = "HELLO";
-            Console.WriteLine(songLyrics.StartsWith(firstSubString.ToLower()));
-            Console.WriteLine(songLyrics.EndsWith(secondSubString.ToLower()));
-        }
+        // public static void Main(string[] args)
+        // {
+        //     // string firstFriend = "Scott";
+        //     // string secondFriend = "Kendra";
+        //     //
+        //     // Console.WriteLine($"My friends are {firstFriend} and {secondFriend}");
+        //     //
+        //     // Console.WriteLine($"The name {firstFriend} has {firstFriend.Length} letters");
+        //     // Console.WriteLine($"The name {secondFriend} has {secondFriend.Length} letters");
+        //
+        //     // string greeting = "     Hello, World!     ";
+        //     // Console.WriteLine($"[{greeting}]");
+        //     //
+        //     // string trimmedGreeting = greeting.TrimStart();
+        //     // Console.WriteLine($"[{trimmedGreeting}]");
+        //     //
+        //     // trimmedGreeting = greeting.TrimEnd();
+        //     // Console.WriteLine($"[{trimmedGreeting}]");
+        //     //
+        //     // trimmedGreeting = greeting.Trim();
+        //     // Console.WriteLine($"[{trimmedGreeting}]");
+        //
+        //     // string sayHello = "Hello, World!";
+        //     // Console.WriteLine(sayHello);
+        //     // sayHello = sayHello.Replace("Hello", "Greetings");
+        //     // Console.WriteLine(sayHello);
+        //     //
+        //     // sayHello = sayHello.Replace("Something else", "Greetings");
+        //     // Console.WriteLine(sayHello);
+        //     //
+        //     // Console.WriteLine(sayHello.ToUpper());
+        //     // Console.WriteLine(sayHello.ToLower());
+        //
+        //     // string songLyrics = "You say goodbye, and I say hello";
+        //     //
+        //     // Console.WriteLine(songLyrics.Contains("goodbye"));
+        //     // Console.WriteLine(songLyrics.Contains("greetings"));
+        //     //
+        //     // string newSongLyrics = "You say greetings, and I say hello";
+        //     //
+        //     // Console.WriteLine(newSongLyrics.Contains("goodbye"));
+        //     // Console.WriteLine(newSongLyrics.Contains("greetings"));
+        //     
+        //     string songLyrics = "You say goodbye, and I say hello";
+        //     
+        //     Console.WriteLine(songLyrics.StartsWith("You"));
+        //     Console.WriteLine(songLyrics.EndsWith("hello"));
+        //     
+        //     Console.WriteLine(songLyrics.StartsWith("you"));
+        //     Console.WriteLine(songLyrics.EndsWith("hello"));
+        //
+        //     var firstSubString = "You";
+        //     var secondSubString = "HELLO";
+        //     Console.WriteLine(songLyrics.StartsWith(firstSubString.ToLower()));
+        //     Console.WriteLine(songLyrics.EndsWith(secondSubString.ToLower()));
+        // }
     }
 }
