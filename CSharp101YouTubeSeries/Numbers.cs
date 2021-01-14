4be7436404eba059e009ca36b31713eff2816fa5
David Guimarães <davidguimaraes@digix.com.br>
2021-01-13 20:36:43 -0400

    06 - Numbers and Integer Precision.

--- CSharp101YouTubeSeries/Numbers.cs
+++ CSharp101YouTubeSeries/Numbers.cs
@@ -21,14 +21,57 @@ namespace CSharp101YouTubeSeries
             // Console.WriteLine(f);    //2
             // Console.WriteLine(g);    //0
             
-            int a = 10;
-            int b = 5;
-            int c = 2;
-            int d = a + b * c;
-            int e = (a + b) * c;
+            // int a = 10;
+            // int b = 5;
+            // int c = 2;
+            // int d = a + b * c;
+            // int e = (a + b) * c;
+            //
+            // Console.WriteLine(d);    //20
+            // Console.WriteLine(e);    //30
             
-            Console.WriteLine(d);    //20
-            Console.WriteLine(e);    //30
+            // int a = 7;
+            // int b = 4;
+            // int c = 3;
+            // int d = (a + b) / c;
+            // int e = (a + b) % c;
+            //
+            // Console.WriteLine($"quotient: {d}");    //3
+            // Console.WriteLine($"remainder: {e}");    //2
+
+            // int min = int.MinValue;
+            // int max = int.MaxValue;
+            //
+            // Console.WriteLine($"The range of integers is {min} to {max}");    //The range of integers is -2147483648 to 2147483647
+            //
+            // int what = max + 3;
+            // Console.WriteLine($"An example of overflow: {what}");    //An example of overflow: -2147483646
+            
+            // double a = 7;
+            // double b = 4;
+            // double c = 3;
+            // double d = (a + b) / c;
+            //
+            // Console.WriteLine(d);    //3.6666666666666665
+            
+            // int a = 7;
+            // int b = 4;
+            // int c = 3;
+            // double d = (a + b) / c;
+            //
+            // Console.WriteLine(d);    //3
+            
+            // var a = 7d;
+            // var b = 4d;
+            // var c = 3d;
+            // var d = (a + b) / c;
+            //
+            // Console.WriteLine(d);    //3.6666666666666665
+            
+            var min = double.MinValue;
+            var max = double.MaxValue;
+            
+            Console.WriteLine($"The range of double is {min} to {max}");    //The range of integers is -2147483648 to 2147483647
         }
     }
 }
