8aabf67869aee21ed3d5c1df97fd03388a53d5d3
David Guimarães <davidguimaraes@digix.com.br>
2021-01-16 18:40:11 -0400

    10 - What are Loops?

--- CSharp101YouTubeSeries/Branches.cs
+++ CSharp101YouTubeSeries/Branches.cs
@@ -4,52 +4,52 @@ namespace CSharp101YouTubeSeries
 {
     public class Branches
     {
-        public static int Main(string[] args)
-        {
-            // int a = 5;
-            // int b = 6;
-            // if (a + b > 10)
-            // {
-            //     Console.WriteLine("The answer is greater than 10.");
-            // }
-            
-            // int c = 5;
-            // int d = 3;
-            //
-            // bool something = c + d > 10;
-            
-            // if (something)
-            //     Console.WriteLine("The answer is greater than 10.");
-            // else
-            //     Console.WriteLine("The answer is NOT greater than 10.");
-
-            //Additional thing that's not been commented on the tutorial
-            // var lambda = new Action(() => Console.WriteLine("The answer is greater than 10."));
-            // var lambda2 = new Action(() => Console.WriteLine("The answer is NOT greater than 10."));
-            //
-            // var action = something ? lambda : lambda2;
-            //
-            // action();
-
-            int a = 5;
-            int b = 3;
-            int c = 4;
-
-            if ((a + b + c > 10) && (a == b))
-            {
-                Console.WriteLine("Both conditions are true.");
-            }
-            
-            if (
-                (a + b + c > 10)
-                || 
-                (a == b)
-                )
-            {
-                Console.WriteLine("At least one of the conditions is true.");
-            }
-
-            return 0;
-        }
+        // public static int Main(string[] args)
+        // {
+        //     // int a = 5;
+        //     // int b = 6;
+        //     // if (a + b > 10)
+        //     // {
+        //     //     Console.WriteLine("The answer is greater than 10.");
+        //     // }
+        //     
+        //     // int c = 5;
+        //     // int d = 3;
+        //     //
+        //     // bool something = c + d > 10;
+        //     
+        //     // if (something)
+        //     //     Console.WriteLine("The answer is greater than 10.");
+        //     // else
+        //     //     Console.WriteLine("The answer is NOT greater than 10.");
+        //
+        //     //Additional thing that's not been commented on the tutorial
+        //     // var lambda = new Action(() => Console.WriteLine("The answer is greater than 10."));
+        //     // var lambda2 = new Action(() => Console.WriteLine("The answer is NOT greater than 10."));
+        //     //
+        //     // var action = something ? lambda : lambda2;
+        //     //
+        //     // action();
+        //
+        //     int a = 5;
+        //     int b = 3;
+        //     int c = 4;
+        //
+        //     if ((a + b + c > 10) && (a == b))
+        //     {
+        //         Console.WriteLine("Both conditions are true.");
+        //     }
+        //     
+        //     if (
+        //         (a + b + c > 10)
+        //         || 
+        //         (a == b)
+        //         )
+        //     {
+        //         Console.WriteLine("At least one of the conditions is true.");
+        //     }
+        //
+        //     return 0;
+        // }
     }
 }
---
+++ CSharp101YouTubeSeries/Loops.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace CSharp101YouTubeSeries
+{
+    public class Loops
+    {
+        public static int Main(string[] args)
+        {
+            int counter = 0;
+            while (counter < 10)
+            {
+                Console.WriteLine($"Hello, World! The counter is {counter}");
+                counter++;
+            }
+
+            counter = 0;
+            do
+            {
+                Console.WriteLine($"Hello, World! The counter is {counter}");
+                counter++;
+            } while (counter < 5);
+
+            for (byte i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"I'm using For Loop! The counter is {i}");
+            }
+            
+            return 0;
+        }
+    }
+}
