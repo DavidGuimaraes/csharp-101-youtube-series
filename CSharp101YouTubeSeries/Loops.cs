61b02c7dc77ff56618165dd4dcb0663c0cad0511
David Guimarães <davidguimaraes@digix.com.br>
2021-01-16 19:32:22 -0400

    11 - Combining Branches and Loops

--- CSharp101YouTubeSeries/Loops.cs
+++ CSharp101YouTubeSeries/Loops.cs
@@ -6,23 +6,31 @@ namespace CSharp101YouTubeSeries
     {
         public static int Main(string[] args)
         {
-            int counter = 0;
-            while (counter < 10)
-            {
-                Console.WriteLine($"Hello, World! The counter is {counter}");
-                counter++;
-            }
+            // int counter = 0;
+            // while (counter < 10)
+            // {
+            //     Console.WriteLine($"Hello, World! The counter is {counter}");
+            //     counter++;
+            // }
+            //
+            // counter = 0;
+            // do
+            // {
+            //     Console.WriteLine($"Hello, World! The counter is {counter}");
+            //     counter++;
+            // } while (counter < 5);
+            //
+            // for (byte i = 0; i < 10; i++)
+            // {
+            //     Console.WriteLine($"I'm using For Loop! The counter is {i}");
+            // }
 
-            counter = 0;
-            do
+            for (int i = 0; i <= 10; i++)
             {
-                Console.WriteLine($"Hello, World! The counter is {counter}");
-                counter++;
-            } while (counter < 5);
-
-            for (byte i = 0; i < 10; i++)
-            {
-                Console.WriteLine($"I'm using For Loop! The counter is {i}");
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine($"{i} is an even number.");
+                }
             }
             
             return 0;
