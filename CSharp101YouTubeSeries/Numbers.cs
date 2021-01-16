05e36375a8d7ae31a4cd43a4c8cce4ed40490bf9
David Guimarães <davidguimaraes@digix.com.br>
2021-01-15 20:30:39 -0400

    07 - Numbers and Decimals.

--- CSharp101YouTubeSeries/Numbers.cs
+++ CSharp101YouTubeSeries/Numbers.cs
@@ -68,10 +68,58 @@ namespace CSharp101YouTubeSeries
             //
             // Console.WriteLine(d);    //3.6666666666666665
             
-            var min = double.MinValue;
-            var max = double.MaxValue;
+            // var min = double.MinValue;
+            // var max = double.MaxValue;
+            //
+            // Console.WriteLine($"The range of double is {min} to {max}");    //The range of integers is -2147483648 to 2147483647
             
-            Console.WriteLine($"The range of double is {min} to {max}");    //The range of integers is -2147483648 to 2147483647
+            // var min = decimal.MinValue;
+            // var max = decimal.MaxValue;
+            //
+            // Console.WriteLine($"The range of decimal is {min} to {max}");    //The range of decimal is -79228162514264337593543950335 to 79228162514264337593543950335
+            //
+            // double a = 1.0;
+            // double b = 3.0;
+            // Console.WriteLine(a/b);    //0.3333333333333333
+            //
+            // decimal c = 1.0m;
+            // decimal d = 3.0m;
+            // Console.WriteLine(c/d);    //0.3333333333333333333333333333
+
+            byte byteMin = byte.MinValue;
+            byte byteMax = byte.MaxValue;
+            
+            Console.WriteLine($"The range of byte is {byteMin} to {byteMax}");    //The range of byte is 0 to 255
+            
+            short shortMin = short.MinValue;
+            short shortMax = short.MaxValue;
+            
+            Console.WriteLine($"The range of short is {shortMin} to {shortMax}");    //The range of short is -32768 to 32767
+            
+            int intMin = int.MinValue;
+            int intMax = int.MaxValue;
+            
+            Console.WriteLine($"The range of int is {intMin} to {intMax}");    //The range of int is -2147483648 to 2147483647
+            
+            long longMin = long.MinValue;
+            long longMax = long.MaxValue;
+            
+            Console.WriteLine($"The range of long is {longMin} to {longMax}");    //The range of long is -9223372036854775808 to 9223372036854775807
+            
+            decimal decimalMin = decimal.MinValue;
+            decimal decimalMax = decimal.MaxValue;
+            
+            Console.WriteLine($"The range of decimal is {decimalMin} to {decimalMax}");    //The range of decimal is -79228162514264337593543950335 to 79228162514264337593543950335
+            
+            float floatMin = float.MinValue;
+            float floatMax = float.MaxValue;
+            
+            Console.WriteLine($"The range of float is {floatMin} to {floatMax}");    //The range of float is -3.4028235E+38 to 3.4028235E+38
+            
+            double doubleMin = double.MinValue;
+            double doubleMax = double.MaxValue;
+            
+            Console.WriteLine($"The range of double is {doubleMin} to {doubleMax}");    //The range of double is -1.7976931348623157E+308 to 1.7976931348623157E+308
         }
     }
 }
