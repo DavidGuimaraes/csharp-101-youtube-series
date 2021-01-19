5a84df22c2fc9b082fefc91a3a4617cdd80078e8
David Guimarães <davidguimaraes@digix.com.br>
2021-01-19 14:17:56 -0400

    14 - Lists of Other Types - playing with the Fibonacci sequence to try List of integers in C#.

--- CSharp101YouTubeSeries/Collections.cs
+++ CSharp101YouTubeSeries/Collections.cs
@@ -5,9 +5,14 @@ namespace CSharp101YouTubeSeries
 {
     public class Collections
     {
+
+        private static List<int> _fibonacciSequence { get; set; }
+        private static int _previous;
+        private static int _previous2;
+        
         public static void Main(string[] args)
         {
-            var names = new List<string> {"<name>", "David", "Amanda" };
+            // var names = new List<string> {"<name>", "David", "Amanda" };
         //     foreach (var name in names)
         //     {
         //         Console.WriteLine($"Hello, {name}!");
@@ -34,17 +39,61 @@ namespace CSharp101YouTubeSeries
         //
         //     names.ForEach(name => Console.WriteLine(name));
 
-            var name = "David";
-            var index = names.IndexOf(name);
+            // var name = "David";
+            // var index = names.IndexOf(name);
+            //
+            // if (index == -1)
+            //     Console.WriteLine($"{name} was NOT found!");
+            // else
+            //     Console.WriteLine($"{name} was found at index {index}");
+            //
+            // names.Sort();
+            //
+            // names.ForEach(x => Console.WriteLine(x));
 
-            if (index == -1)
-                Console.WriteLine($"{name} was NOT found!");
-            else
-                Console.WriteLine($"{name} was found at index {index}");
+            // var fibbonaciNumbers = new List<int> { 1, 1 };
+
+            // var previous = fibbonaciNumbers[fibbonaciNumbers.Count - 1];
+            // var previous = fibbonaciNumbers[^1];
+            // var previous2 = fibbonaciNumbers[^2];
+            //
+            // fibbonaciNumbers.Add(previous + previous2);
+            //
+            // fibbonaciNumbers.ForEach(Console.WriteLine);
             
-            names.Sort();
+            Challenge();
+        }
+
+        private static void InitializeFibonacciSequence()
+        {
+            _fibonacciSequence = new List<int> {1, 1};
+        }
+
+        private static void Calculate()
+        {
+            _previous = _fibonacciSequence[^1];
+            _previous2 = _fibonacciSequence[^2];
             
-            names.ForEach(x => Console.WriteLine(x));
+            _fibonacciSequence.Add(_previous + _previous2);
+        }
+
+        private static void PrintFibonacciNumbers()
+        {
+            _fibonacciSequence.ForEach(Console.WriteLine);
+        }
+
+        private static void Challenge()
+        {
+            InitializeFibonacciSequence();
+
+            var count = 1;
+            while (count <= 18)
+            {
+                Calculate();
+                count++;
+            }
+
+            PrintFibonacciNumbers();
         }
     }
 }
