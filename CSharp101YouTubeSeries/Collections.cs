8f27ef27f9446404bb0c7ab8f3b59305adc49d08
David Guimarães <davidguimaraes@digix.com.br>
2021-01-19 11:57:47 -0400

    12 - Arrays, List, and Collections

---
+++ CSharp101YouTubeSeries/Collections.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CSharp101YouTubeSeries
+{
+    public class Collections
+    {
+        public static void Main(string[] args)
+        {
+            var names = new List<string> {"<name>", "David", "Amanda" };
+            foreach (var name in names)
+            {
+                Console.WriteLine($"Hello, {name}!");
+            }
+
+            for (int i = 0; i <= names.Count-1; i++) {
+                Console.WriteLine($"Hello, {names[i]}!");
+            }
+
+            List<string> newNames = new List<string>();
+            newNames.Add("Nessa");
+            newNames.Add("Téia");
+
+            foreach (var newName in newNames)
+            {
+                Console.WriteLine(newName);
+            }
+
+            names.Remove("<name>");
+            foreach (var name in names)
+            {
+                Console.WriteLine(name);
+            }
+
+            names.ForEach(name => Console.WriteLine(name));
+        }
+    }
+}
--- CSharp101YouTubeSeries/Loops.cs
+++ CSharp101YouTubeSeries/Loops.cs
@@ -4,36 +4,36 @@ namespace CSharp101YouTubeSeries
 {
     public class Loops
     {
-        public static int Main(string[] args)
-        {
-            // int counter = 0;
-            // while (counter < 10)
-            // {
-            //     Console.WriteLine($"Hello, World! The counter is {counter}");
-            //     counter++;
-            // }
-            //
-            // counter = 0;
-            // do
-            // {
-            //     Console.WriteLine($"Hello, World! The counter is {counter}");
-            //     counter++;
-            // } while (counter < 5);
-            //
-            // for (byte i = 0; i < 10; i++)
-            // {
-            //     Console.WriteLine($"I'm using For Loop! The counter is {i}");
-            // }
-
-            for (int i = 0; i <= 10; i++)
-            {
-                if (i % 2 == 0)
-                {
-                    Console.WriteLine($"{i} is an even number.");
-                }
-            }
-            
-            return 0;
-        }
+        // public static int Main(string[] args)
+        // {
+        //     // int counter = 0;
+        //     // while (counter < 10)
+        //     // {
+        //     //     Console.WriteLine($"Hello, World! The counter is {counter}");
+        //     //     counter++;
+        //     // }
+        //     //
+        //     // counter = 0;
+        //     // do
+        //     // {
+        //     //     Console.WriteLine($"Hello, World! The counter is {counter}");
+        //     //     counter++;
+        //     // } while (counter < 5);
+        //     //
+        //     // for (byte i = 0; i < 10; i++)
+        //     // {
+        //     //     Console.WriteLine($"I'm using For Loop! The counter is {i}");
+        //     // }
+        //
+        //     for (int i = 0; i <= 10; i++)
+        //     {
+        //         if (i % 2 == 0)
+        //         {
+        //             Console.WriteLine($"{i} is an even number.");
+        //         }
+        //     }
+        //     
+        //     return 0;
+        // }
     }
 }
