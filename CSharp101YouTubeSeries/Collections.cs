b13d5499f6cdc9a0b248ab37d4dbe283820ce7a0
David Guimarães <davidguimaraes@digix.com.br>
2021-01-19 12:11:06 -0400

    13 - Sort, Search, and Index Lists

--- CSharp101YouTubeSeries/Collections.cs
+++ CSharp101YouTubeSeries/Collections.cs
@@ -8,31 +8,43 @@ namespace CSharp101YouTubeSeries
         public static void Main(string[] args)
         {
             var names = new List<string> {"<name>", "David", "Amanda" };
-            foreach (var name in names)
-            {
-                Console.WriteLine($"Hello, {name}!");
-            }
+        //     foreach (var name in names)
+        //     {
+        //         Console.WriteLine($"Hello, {name}!");
+        //     }
+        //
+        //     for (int i = 0; i <= names.Count-1; i++) {
+        //         Console.WriteLine($"Hello, {names[i]}!");
+        //     }
+        //
+        //     List<string> newNames = new List<string>();
+        //     newNames.Add("Nessa");
+        //     newNames.Add("Téia");
+        //
+        //     foreach (var newName in newNames)
+        //     {
+        //         Console.WriteLine(newName);
+        //     }
+        //
+        //     names.Remove("<name>");
+        //     foreach (var name in names)
+        //     {
+        //         Console.WriteLine(name);
+        //     }
+        //
+        //     names.ForEach(name => Console.WriteLine(name));
 
-            for (int i = 0; i <= names.Count-1; i++) {
-                Console.WriteLine($"Hello, {names[i]}!");
-            }
+            var name = "David";
+            var index = names.IndexOf(name);
 
-            List<string> newNames = new List<string>();
-            newNames.Add("Nessa");
-            newNames.Add("Téia");
-
-            foreach (var newName in newNames)
-            {
-                Console.WriteLine(newName);
-            }
-
-            names.Remove("<name>");
-            foreach (var name in names)
-            {
-                Console.WriteLine(name);
-            }
-
-            names.ForEach(name => Console.WriteLine(name));
+            if (index == -1)
+                Console.WriteLine($"{name} was NOT found!");
+            else
+                Console.WriteLine($"{name} was found at index {index}");
+            
+            names.Sort();
+            
+            names.ForEach(x => Console.WriteLine(x));
         }
     }
 }
